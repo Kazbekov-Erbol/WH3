c0bc10a7407d8feb1fc0bbd7a4792059147859b8
Kazbekov-Erbol <didiamante76@gmail.com>
2024-10-07 23:21:28 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] numbers = {15.3, 12.5, -23.1, 34.0, -23.5,
+                12.4, 5.3, 23.5, -14.3, -4.1, 2.0, -1.2, 7.2, -45.1, 32.1};
+        double sum = 0;
+        int quantity = 0;
+        boolean isThisFirstNegative = false;
+        for (double element : numbers) {
+            if (isThisFirstNegative) {
+                if (element > 0) {
+                    sum += element;
+                    quantity++;
+                }
+            } else if (element < 0) {
+                isThisFirstNegative = true;
+            }
+        }
+        if (quantity > 0) {
+            double average = sum / quantity;
+            System.out.println("Среднее арифметическое положительных чисел после отрицательного числа" + average);
+        }else {
+            System.out.println("положительные числа после первого отрицательного числа не найдены");
+        }
+
+    }
+}
